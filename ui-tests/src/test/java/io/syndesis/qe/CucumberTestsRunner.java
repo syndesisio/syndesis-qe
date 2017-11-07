cdb9056c94046a4269889bceb0f90d567ff4b764
Martin Stepanek <mastepan@localhost.localdomain>
2017-11-07 15:12:53 +0100

    [ui-tests] Fix Login test

--- ui-tests/src/test/java/io/syndesis/qe/CucumberTestsRunner.java
+++ ui-tests/src/test/java/io/syndesis/qe/CucumberTestsRunner.java
@@ -1,3 +1,5 @@
+package io.syndesis.qe;
+
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 import org.junit.BeforeClass;
@@ -9,26 +11,23 @@ import com.codeborne.selenide.Configuration;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import properties.PropertiesLoader;
+import io.syndesis.qe.configuration.TestConfiguration;
 
-/**
- * Created by mastepan on 10/18/17.
- */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/test/java/features",
-		glue={"steps"},
-		monochrome=true,
-		format={"pretty"}
+		features = "src/test/java/io/syndesis/qe/features",
+		glue = {"io/syndesis/qe/steps"},
+		monochrome = true,
+		format = {"pretty"}
 )
 public class CucumberTestsRunner {
 	// setup
 	@BeforeClass
 	public static void setup() {
-		//load properties
-		PropertiesLoader.load(System.getProperty("propertiesFileAbsolutePath"));
+		//load test configuration properties
+		TestConfiguration.loadFromFile(System.getProperty("propertiesFileAbsolutePath"));
 		//set up Selenide
 		Configuration.timeout = 60000;
-		getWebDriver().manage().window().setSize(new Dimension(1920,1024));
+		getWebDriver().manage().window().setSize(new Dimension(1920, 1024));
 	}
 }
