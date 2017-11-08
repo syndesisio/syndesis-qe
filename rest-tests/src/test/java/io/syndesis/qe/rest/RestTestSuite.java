e988b3540b2c2ac45cf2d14a3a518fb29bdfe834
David Simansky <dsimansk@redhat.com>
2017-11-08 11:06:07 +0100

    chore: Plug ui tests to new config structure

--- rest-tests/src/test/java/io/syndesis/qe/rest/RestTestSuite.java
+++ rest-tests/src/test/java/io/syndesis/qe/rest/RestTestSuite.java
@@ -4,12 +4,12 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import io.syndesis.qe.rest.tests.ClientTest;
+import io.syndesis.qe.rest.tests.integrations.TwitterSalesforceTest;
 import io.syndesis.qe.templates.SyndesisTemplate;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	ClientTest.class
+		TwitterSalesforceTest.class
 })
 public class RestTestSuite {
 
