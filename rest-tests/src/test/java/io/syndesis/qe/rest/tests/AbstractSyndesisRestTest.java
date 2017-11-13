1939a3de2deb83adaa9e4a7901196d6b35fe0e0f
David Simansky <dsimansk@redhat.com>
2017-11-13 08:27:32 +0100

    chore: Allign loggign config and dependecies
    fix: Remove obsolete variables
    
    fix: Add missing logging deps

--- rest-tests/src/test/java/io/syndesis/qe/rest/tests/AbstractSyndesisRestTest.java
+++ rest-tests/src/test/java/io/syndesis/qe/rest/tests/AbstractSyndesisRestTest.java
@@ -1,14 +1,7 @@
 package io.syndesis.qe.rest.tests;
 
-import java.nio.file.Paths;
-
-import io.fabric8.kubernetes.client.Config;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.openshift.client.DefaultOpenShiftClient;
-import io.fabric8.openshift.client.OpenShiftClient;
 import io.restassured.RestAssured;
-import io.syndesis.qe.accounts.Account;
-import io.syndesis.qe.accounts.AccountsDirectory;
+import io.syndesis.qe.TestConfiguration;
 import io.syndesis.qe.utils.SyndesisRestConstants;
 
 /**
@@ -20,34 +13,13 @@ import io.syndesis.qe.utils.SyndesisRestConstants;
  */
 public abstract class AbstractSyndesisRestTest {
 
-	private static Account syndesisAccount;
-	private static Account openshiftAccount;
-	private static AccountsDirectory accountsDirectory;
-
 	protected static String syndesisURL;
-	protected static String openshiftToken;
-	protected static String openshiftUrl;
-	protected static OpenShiftClient openshiftClient;
 
 	public AbstractSyndesisRestTest() {
 
-		accountsDirectory = new AccountsDirectory(Paths.get(SyndesisRestConstants.ACCOUNT_CONFIG_PATH));
+		syndesisURL = TestConfiguration.syndesisUrl();
 
-		syndesisAccount = accountsDirectory.getAccount("syndesis").get();
-		openshiftAccount = accountsDirectory.getAccount("openshift").get();
-
-		syndesisURL = syndesisAccount.getProperty("instanceUrl");
-		openshiftUrl = openshiftAccount.getProperty("instanceUrl");
-		openshiftToken = openshiftAccount.getProperty("openshiftToken");
 		RestAssured.baseURI = syndesisURL;
 		RestAssured.basePath = SyndesisRestConstants.API_PATH;
-
-		final Config config = new ConfigBuilder()
-				.withUsername(syndesisAccount.getProperty("login"))
-				.withPassword(syndesisAccount.getProperty("password"))
-				.withMasterUrl(openshiftUrl)
-				.build();
-
-		openshiftClient = new DefaultOpenShiftClient(config);
 	}
 }
