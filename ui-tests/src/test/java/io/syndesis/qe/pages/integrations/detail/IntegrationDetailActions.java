6cac8ae480e4df9f434556901261b8a52dc0f1c0
Lukas Vydra <lvydra@redhat.com>
2017-11-20 14:29:20 +0100

    Integrations detail page objects

---
+++ ui-tests/src/test/java/io/syndesis/qe/pages/integrations/detail/IntegrationDetailActions.java
@@ -0,0 +1,8 @@
+package io.syndesis.qe.pages.integrations.detail;
+
+public final class IntegrationDetailActions {
+	public static final String START = "Start Integration";
+	public static final String STOP = "Stop Integration";
+	public static final String DELETE = "Delete Integration";
+	public static final String EDIT = "Edit Integration";
+}
