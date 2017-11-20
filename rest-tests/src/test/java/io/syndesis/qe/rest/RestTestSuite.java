b2279c2b38aff483f5c549a94fe36942328d2159
Tomas Plevko <tplevko@redhat.com>
2017-11-20 15:32:47 +0100

    [relates #103] Create SF-DB rest test

--- rest-tests/src/test/java/io/syndesis/qe/rest/RestTestSuite.java
+++ rest-tests/src/test/java/io/syndesis/qe/rest/RestTestSuite.java
@@ -4,12 +4,14 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import io.syndesis.qe.rest.tests.integrations.SalesforceDbTest;
 import io.syndesis.qe.rest.tests.integrations.TwitterSalesforceTest;
 import io.syndesis.qe.templates.SyndesisTemplate;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-		TwitterSalesforceTest.class
+	TwitterSalesforceTest.class,
+	SalesforceDbTest.class
 })
 public class RestTestSuite {
 
