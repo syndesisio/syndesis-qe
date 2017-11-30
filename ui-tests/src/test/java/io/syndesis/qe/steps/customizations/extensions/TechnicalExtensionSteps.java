fac0bb8860747678b9a0d16406152dad0083933d
lvydra <lvydra@redhat.com>
2017-11-30 15:29:24 +0100

    Tech extensions scenario prototype (#205)
    * Tech extensions scenario prototype
    
    * Technical extension steps

---
+++ ui-tests/src/test/java/io/syndesis/qe/steps/customizations/extensions/TechnicalExtensionSteps.java
@@ -0,0 +1,62 @@
+package io.syndesis.qe.steps.customizations.extensions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TechnicalExtensionSteps {
+
+    @When("^click on the \"([^\"]*)\" tab$")
+    public void click_on_the_tab_new_step(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^she is presented with dialog page \"([^\"]*)\"$")
+    public void she_is_presented_with_dialog_page_new_step(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Camilla choose a file \"([^\"]*)\"$")
+    public void camilla_choose_a_file(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^she is presented with file \"([^\"]*)\" being apploaded$")
+    public void she_is_presented_with_file_being_apploaded_new_step(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^she can review uploaded technical extension$")
+    public void she_can_review_uploaded_technical_extension_new_step() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^technical extension \"([^\"]*)\" is present in technical extensions list$")
+    public void technical_extension_is_present_in_technical_extensions_list_new_step(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^she can see \"([^\"]*)\" in step list$")
+    public void she_can_see_in_step_list_new_step(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^she can see integrations \"([^\"]*)\" in which is tech extension used$")
+    public void she_can_see_integrations_in_which_is_tech_extension_used_new_step(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Camilla can not see \"([^\"]*)\" technical extension anymore$")
+    public void camilla_can_not_see_technical_extension_anymore_new_step(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
