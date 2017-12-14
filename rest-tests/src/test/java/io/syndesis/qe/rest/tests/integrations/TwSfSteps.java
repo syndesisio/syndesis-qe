a2693e422f9ffd09e129e713e7c961d5499949f6
Tomas Plevko <tplevko@redhat.com>
2017-12-14 18:16:15 +0100

    Rest update to use cucumber, verification steps moved to utils (#222)

---
+++ rest-tests/src/test/java/io/syndesis/qe/rest/tests/integrations/TwSfSteps.java
@@ -0,0 +1,111 @@
+package io.syndesis.qe.rest.tests.integrations;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import io.syndesis.model.connection.Connection;
+import io.syndesis.model.connection.Connector;
+import io.syndesis.model.filter.FilterPredicate;
+import io.syndesis.model.filter.RuleFilterStep;
+import io.syndesis.model.integration.Integration;
+import io.syndesis.model.integration.SimpleStep;
+import io.syndesis.model.integration.Step;
+import io.syndesis.qe.endpoints.ConnectionsEndpoint;
+import io.syndesis.qe.endpoints.ConnectorsEndpoint;
+import io.syndesis.qe.endpoints.IntegrationsEndpoint;
+import io.syndesis.qe.utils.FilterRulesBuilder;
+import io.syndesis.qe.utils.RestConstants;
+import io.syndesis.qe.utils.TestUtils;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Steps for Twitter mention to Salesforce upsert contact integration.
+ *
+ * Oct 7, 2017 Red Hat
+ *
+ * @author tplevko@redhat.com
+ */
+@Slf4j
+public class TwSfSteps {
+
+	public static final String SYNDESIS_TALKY_ACCOUNT = "twitter_talky";
+
+	private final ConnectionsEndpoint connectionsEndpoint;
+	private final ConnectorsEndpoint connectorsEndpoint;
+	private final IntegrationsEndpoint integrationsEndpoint;
+	private final List<Step> steps = new ArrayList<>();
+
+	public TwSfSteps() throws GeneralSecurityException {
+		connectorsEndpoint = new ConnectorsEndpoint(RestConstants.getInstance().getSyndesisURL());
+		connectionsEndpoint = new ConnectionsEndpoint(RestConstants.getInstance().getSyndesisURL());
+		integrationsEndpoint = new IntegrationsEndpoint(RestConstants.getInstance().getSyndesisURL());
+	}
+
+	@Given("^creates TW mention step with \"([^\"]*)\" action")
+	public void createTwitterStep(String twitterAction) {
+
+		final Connector twitterConnector = connectorsEndpoint.get("twitter");
+		final Connection twitterConnection = connectionsEndpoint.get(RestConstants.getInstance().getTWITTER_CONNECTION_ID());
+		final Step twitterStep = new SimpleStep.Builder()
+				.stepKind("endpoint")
+				.connection(twitterConnection)
+				.action(TestUtils.findConnectorAction(twitterConnector, twitterAction))
+				.build();
+		steps.add(twitterStep);
+	}
+
+	@Given("^creates TW to SF mapper step")
+	public void createMapperStep() throws IOException {
+		final String mapping = new String(Files.readAllBytes(Paths.get("./target/test-classes/mappings/twitter-salesforce.json")));
+
+		final Step mapperStep = new SimpleStep.Builder()
+				.stepKind("mapper")
+				.configuredProperties(TestUtils.map("atlasmapping", mapping))
+				.build();
+		steps.add(mapperStep);
+	}
+
+	@Given("^creates basic TW to SF filter step")
+	public void createBasicFilterStep() {
+		final Step basicFilter = new RuleFilterStep.Builder()
+				.configuredProperties(TestUtils.map(
+						"predicate", FilterPredicate.AND.toString(),
+						"rules", new FilterRulesBuilder().addPath("text").addValue("#backendTest").addOps("contains").build()
+				))
+				.build();
+		steps.add(basicFilter);
+	}
+
+	@Given("^creates SF step for TW SF test")
+	public void createSalesforceStep() {
+		final Connector salesforceConnector = connectorsEndpoint.get("salesforce");
+
+		final Connection salesforceConnection = connectionsEndpoint.get(RestConstants.getInstance().getSALESFORCE_CONNECTION_ID());
+		final Step salesforceStep = new SimpleStep.Builder()
+				.stepKind("endpoint")
+				.connection(salesforceConnection)
+				.action(TestUtils.findConnectorAction(salesforceConnector, "salesforce-create-sobject"))
+				.build();
+		steps.add(salesforceStep);
+	}
+
+	@When("^creates TW to SF integration with name: \"([^\"]*)\"$")
+	public void createIntegrationFromGivenSteps(String integrationName) throws GeneralSecurityException {
+
+		Integration integration = new Integration.Builder()
+				.steps(steps)
+				.name(integrationName)
+				.desiredStatus(Integration.Status.Activated)
+				.build();
+
+		log.info("Creating integration {}", integration.getName());
+		integration = integrationsEndpoint.create(integration);
+	}
+}
---
+++ utilities/src/main/java/io/syndesis/qe/validation/SfDbValidationSteps.java
@@ -0,0 +1,141 @@
+package io.syndesis.qe.validation;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import org.hamcrest.CoreMatchers;
+
+import com.force.api.ApiConfig;
+import com.force.api.ForceApi;
+import com.force.api.QueryResult;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import io.syndesis.qe.accounts.Account;
+import io.syndesis.qe.accounts.AccountsDirectory;
+import io.syndesis.qe.endpoints.TestSupport;
+import io.syndesis.qe.salesforce.Lead;
+import io.syndesis.qe.utils.DbUtils;
+import io.syndesis.qe.utils.RestConstants;
+import io.syndesis.qe.utils.SampleDbConnectionManager;
+import io.syndesis.qe.utils.TestUtils;
+import lombok.extern.slf4j.Slf4j;
+import twitter4j.TwitterException;
+
+/**
+ * Validation steps for Salesforce to database integrations.
+ *
+ * Dec 11, 2017 Red Hat
+ *
+ * @author tplevko@redhat.com
+ */
+@Slf4j
+public class SfDbValidationSteps {
+
+	private final DbUtils dbUtils;
+	private final ForceApi salesforce;
+	private final AccountsDirectory accountsDirectory;
+
+	public SfDbValidationSteps() throws IOException, TwitterException {
+		accountsDirectory = new AccountsDirectory();
+		final Account salesforceAccount = accountsDirectory.getAccount("salesforce").get();
+		dbUtils = new DbUtils(SampleDbConnectionManager.getInstance().getConnection());
+		salesforce = new ForceApi(new ApiConfig()
+				.setClientId(salesforceAccount.getProperty("clientId"))
+				.setClientSecret(salesforceAccount.getProperty("clientSecret"))
+				.setUsername(salesforceAccount.getProperty("userName"))
+				.setPassword(salesforceAccount.getProperty("password"))
+				.setForceURL(salesforceAccount.getProperty("loginUrl")));
+	}
+
+	@Given("^cleans before SF to DB, removes user with first name: \"([^\"]*)\" and last name: \"([^\"]*)\"")
+	public void cleanupSfDb(String firstName, String lastName) throws TwitterException {
+		TestSupport.getInstance().resetDB(RestConstants.getInstance().getSyndesisURL());
+		deleteSalesforceLead(salesforce, firstName, lastName);
+		dbUtils.deleteRecordsInTable(RestConstants.getInstance().getTODO_APP_NAME());
+	}
+
+	@Then("^cleans after SF to DB, removes user with first name: \"([^\"]*)\" and last name: \"([^\"]*)\"")
+	public void tearDownSfDb(String firstName, String lastName) throws TwitterException {
+		cleanupSfDb(firstName, lastName);
+		SampleDbConnectionManager.getInstance().closeConnection();
+	}
+
+	@Then("^validates SF to DB created new lead with first name: \"([^\"]*)\", last name: \"([^\"]*)\", email: \"([^\"]*)\"")
+	public void validateSfDbIntegration(String firstName, String lastName, String emailAddress) {
+		final long start = System.currentTimeMillis();
+		// We wait for exactly 1 record to appear in DB.
+		final boolean contactCreated = TestUtils.waitForEvent(leadCount -> leadCount == 1, () -> dbUtils.getNumberOfRecordsInTable(RestConstants.getInstance().getTODO_APP_NAME()),
+				TimeUnit.MINUTES,
+				2,
+				TimeUnit.SECONDS,
+				5);
+		assertThat("Lead record has appeard in db", contactCreated, is(true));
+		log.info("Lead record appeared in DB. It took {}s to create contact.", TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - start));
+		// Now we verify, the created lead contains the correct personal information.
+		assertThat(getLeadTaskFromDb(firstName, lastName).toLowerCase(), CoreMatchers.containsString(emailAddress));
+	}
+
+	/**
+	 * Used for verification of successful creation of a new task in the todo app.
+	 *
+	 * @return
+	 */
+	private String getLeadTaskFromDb(String firstName, String lastName) {
+
+		String leadTask = null;
+		try (ResultSet rs = dbUtils.executeSqlOnSampleDb("SELECT ID, TASK, COMPLETED FROM todo where task like '%"
+				+ firstName + " " + lastName + "%'");) {
+			if (rs.next()) {
+				leadTask = rs.getString("TASK");
+				log.debug("TASK = " + leadTask);
+			}
+		} catch (SQLException ex) {
+			log.error("Error: " + ex);
+		}
+		return leadTask;
+	}
+
+	@Then("^creates SF lead with first name: \"([^\"]*)\", last name: \"([^\"]*)\", email: \"([^\"]*)\" and company: \"([^\"]*)\"")
+	public void createNewSalesforceLead(String firstName, String lastName, String email, String companyName) {
+		// The table has to be empty prior to testing.
+		assertThat(dbUtils.getNumberOfRecordsInTable(RestConstants.getInstance().getTODO_APP_NAME()), equalTo(0));
+		final Lead lead = new Lead();
+		lead.setFirstName(firstName);
+		lead.setLastName(lastName);
+		lead.setCompany(companyName);
+		lead.setEmail(email);
+
+		final String id = salesforce.createSObject("lead", lead);
+	}
+
+	/**
+	 * Looks for leads with specified first and last name and deletes them if it finds any.
+	 *
+	 * @param salesforce
+	 */
+	private void deleteSalesforceLead(ForceApi salesforce, String firstName, String lastName) {
+		final Optional<Lead> lead = getSalesforceLead(salesforce, firstName, lastName);
+		if (lead.isPresent()) {
+			final String id = String.valueOf(lead.get().getId());
+			salesforce.deleteSObject("lead", id);
+			log.info("Deleting salesforce lead: {}", lead.get());
+			deleteSalesforceLead(salesforce, firstName, lastName);
+		}
+	}
+
+	private Optional<Lead> getSalesforceLead(ForceApi salesforce, String firstName, String lastName) {
+		final QueryResult<Lead> queryResult = salesforce.query("SELECT Id,FirstName,LastName,Email,Company FROM lead where FirstName = '"
+				+ firstName + "' and LastName='" + lastName + "'", Lead.class
+		);
+		final Optional<Lead> lead = queryResult.getTotalSize() > 0 ? Optional.of(queryResult.getRecords().get(0)) : Optional.empty();
+		return lead;
+	}
+}
