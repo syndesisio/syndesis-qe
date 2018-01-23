3a31aa06ebcff018b54f2d5e58816f8d87a7d445
Martin Stepanek <mastepan@localhost.localdomain>
2018-01-23 11:34:12 +0100

    [ui-tests] Fix method names in ApiClientCOnnectorsSteps

--- ui-tests/src/test/java/io/syndesis/qe/steps/customizations/connectors/ApiClientConnectorsSteps.java
+++ ui-tests/src/test/java/io/syndesis/qe/steps/customizations/connectors/ApiClientConnectorsSteps.java
@@ -33,13 +33,13 @@ public class ApiClientConnectorsSteps {
 	private ApiClientConnectorDetail connectorDetailPage = new ApiClientConnectorDetail();
 
 	@Given("^(\\w+) opens \"([^\"]*)\" page$")
-	public void she_opens_api_client_connector_page(String user, String pageName) {
+	public void sheOpensApiClientConnectorPage(String user, String pageName) {
 		customizations.equals("open API Client Connectors page");
 		apiClientConnectorsPage.validate();
 	}
 
 	@And("^(\\w+) navigates to the next Api Connector wizard step \"([^\"]*)\"$")
-	public void she_navigates_to_the_next_Api_Connector_wizard_step(String user, String step) {
+	public void sheNavigatesToTheNextApiConnectorWizardStep(String user, String step) {
 		wizard.nextStep();
 
 		//validation ensures that multiple clicking the Next button doesn't click on the same button multiple times before the next step loads and displays
@@ -47,18 +47,18 @@ public class ApiClientConnectorsSteps {
 	}
 
 	@And("^(\\w+) creates new connector$")
-	public void she_creates_new_connector(String user) {
+	public void sheCreatesNewConnector(String user) {
 		wizard.nextStep();
 	}
 
 	@Then("^(\\w+) opens new Api Connector wizard$")
-	public void she_opens_new_api_connector_wizard(String user) throws Throwable {
+	public void sheOpensNewApiConnectorWizard(String user) throws Throwable {
 		apiClientConnectorsPage.startWizard();
 		wizard = new ApiClientConnectorWizard();
 	}
 
 	@Then("^(\\w+) uploads swagger file$")
-	public void she_uploads_swagger_file(String user, DataTable fileParams) throws Throwable {
+	public void sheUploadsSwaggerFile(String user, DataTable fileParams) throws Throwable {
 		uploadSwaggerWizardStep.validate();
 		List<List<String>> dataRows = fileParams.cells(0);
 		List<String> sourceTypes = new ArrayList<String>();
@@ -73,14 +73,14 @@ public class ApiClientConnectorsSteps {
 	}
 
 	@Then("^(\\w+) reviews swagger actions$")
-	public void she_reviews_swagger_actions(String user) throws Throwable {
+	public void sheReviewsSwaggerActions(String user) throws Throwable {
 		reviewSwaggerActionsWizardStep.validate();
 		reviewSwaggerActionsWizardStep.review();
 		reviewSwaggerActionsWizardStep.nextWizardStep();
 	}
 
 	@Then("(\\w+) sets up security by \"([^\"]*)\"$")
-	public void she_sets_up_security_by(String user, String authType) throws Throwable {
+	public void sheSetsUpSecurityBy(String user, String authType) throws Throwable {
 		securityWizardStep.validate();
 		//String authType, String authenticationUrl, String authorizationUrl, String scopes
 		switch (authType) {
@@ -93,19 +93,19 @@ public class ApiClientConnectorsSteps {
 	}
 
 	@Then("^(\\w+) reviews general info$")
-	public void she_reviews_general_info(String user) throws Throwable {
+	public void sheReviewsGeneralInfo(String user) throws Throwable {
 		generalConnectorInfoWizardStep.validate();
 		generalConnectorInfoWizardStep.review();
 		generalConnectorInfoWizardStep.validate();
 	}
 
 	@Then("(\\w+) is presented with the new connector \"([^\"]*)\"$")
-	public void she_is_presented_with_the_new_connector(String user, String connectorName) throws Throwable {
+	public void sheIsPresentedWithTheNewConnector(String user, String connectorName) throws Throwable {
 		Assert.assertTrue("Connector [" + connectorName + "] is not listed.", apiClientConnectorsPage.isConnectorPresent(connectorName));
 	}
 
 	@Then("^(\\w+) creates new API connector \"([^\"]*)\"$")
-	public void she_creates_new_api_connector(String user, String connectorName, DataTable properties) throws Throwable {
+	public void sheCreatesNewApiConnector(String user, String connectorName, DataTable properties) throws Throwable {
 
 		String securityAuthType = null;
 
@@ -130,38 +130,38 @@ public class ApiClientConnectorsSteps {
 		commonSteps.validatePage(user, "Customizations");
 		commonSteps.clickOnLink("API Client Connectors");
 		commonSteps.validatePage(user, "API Client Connectors");
-		she_opens_new_api_connector_wizard(user);
+		sheOpensNewApiConnectorWizard(user);
 
 		List<String> row = asList("file", "swagger_files/api_client_connector/petstore_swagger.json");
 		List<List<String>> dataTable = new ArrayList<>();
 		dataTable.add(row);
 
-		she_uploads_swagger_file(user, DataTable.create(dataTable));
-		she_navigates_to_the_next_Api_Connector_wizard_step(user, "Review Swagger Action");
-		she_navigates_to_the_next_Api_Connector_wizard_step(user, "Security");
+		sheUploadsSwaggerFile(user, DataTable.create(dataTable));
+		sheNavigatesToTheNextApiConnectorWizardStep(user, "Review Swagger Action");
+		sheNavigatesToTheNextApiConnectorWizardStep(user, "Security");
 
 		List<String> secRow = asList("authType", "authenticationUrl", "https://syndesis.192.168.42.188.xip.io/api/v1/credentials/callback", "scopes");
 		List<List<String>> secDataTable = new ArrayList<>();
 		secDataTable.add(secRow);
 
-		she_sets_up_security_by(user, securityAuthType);
-		she_navigates_to_the_next_Api_Connector_wizard_step(user, "General Connector Info");
-		she_creates_new_connector(user);
-		she_is_presented_with_the_new_connector(user, connectorName);
+		sheSetsUpSecurityBy(user, securityAuthType);
+		sheNavigatesToTheNextApiConnectorWizardStep(user, "General Connector Info");
+		sheCreatesNewConnector(user);
+		sheIsPresentedWithTheNewConnector(user, connectorName);
 	}
 
 	@Then("^(\\w+) is presented with a connectors list of size (\\d+)$")
-	public void she_is_presented_with_an_connectors_list_of_size(String userName, String listSize) {
+	public void sheIsPresentedWithAnConnectorsListOfSize(String userName, String listSize) {
 		Assert.assertTrue("The connectors list should be of size <" + listSize + ">.", apiClientConnectorsPage.isConnectorsListLongAs(Integer.parseInt(listSize)));
 	}
 
 	@Then("^(\\w+) opens the API connector \"([^\"]*)\" detail$")
-	public void she_opens_the_api_connector_detail(String userName, String connectorName) {
+	public void sheOpensTheApiConnectorDetail(String userName, String connectorName) {
 		apiClientConnectorsPage.clickConnectorByTitle(connectorName);
 	}
 
 	@Then("^(\\w+) edits property")
-	public void she_edits_property(String userName, DataTable dataTable) {
+	public void sheEditsProperty(String userName, DataTable dataTable) {
 		for (List<String> data : dataTable.raw()) {
 			String propertyName = data.get(0);
 			String propertyValue = data.get(1);
