efb2dd5f90cf8f4c0f518645d51b1a0dea72f032
Lukas Vydra <lvydra@redhat.com>
2018-02-07 11:33:28 +0100

    Integration detail page object radical reduction

--- ui-tests/src/test/java/io/syndesis/qe/pages/integrations/detail/IntegrationDetailPageDraft.java
+++
@@ -1,8 +0,0 @@
-package io.syndesis.qe.pages.integrations.detail;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class IntegrationDetailPageDraft extends IntegrationDetailPage {
-	public final List<String> actionsSet = Arrays.asList(IntegrationDetailActions.START, IntegrationDetailActions.DELETE, IntegrationDetailActions.EDIT);
-}
--- ui-tests/src/test/java/io/syndesis/qe/pages/integrations/detail/IntegrationDetailPageInProgress.java
+++
@@ -1,8 +0,0 @@
-package io.syndesis.qe.pages.integrations.detail;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class IntegrationDetailPageInProgress extends IntegrationDetailPage {
-	public final List<String> actionsSet = Arrays.asList(IntegrationDetailActions.DELETE, IntegrationDetailActions.EDIT);
-}
