f1e6b16f7ff8f3698716dac255096d2d15b3b3cb
Martin Stepanek <mastepan@localhost.localdomain>
2018-02-13 15:58:33 +0100

    [ui-tests] Fix tabs to spaces - checkstyle

--- ui-tests/src/test/java/io/syndesis/qe/steps/customizations/connectors/ApiClientConnectorsSteps.java
+++ ui-tests/src/test/java/io/syndesis/qe/steps/customizations/connectors/ApiClientConnectorsSteps.java
@@ -15,74 +15,74 @@ import io.syndesis.qe.steps.customizations.connectors.wizard.WizardSteps;
 
 public class ApiClientConnectorsSteps {
 
-	private static ApiClientConnectors apiClientConnectorsPage = new ApiClientConnectors();
+    private static ApiClientConnectors apiClientConnectorsPage = new ApiClientConnectors();
 
-	@Then("^(\\w+) opens new Api Connector wizard$")
-	public void openNewApiConnectorWizard(String user) throws Throwable {
-		apiClientConnectorsPage.startWizard();
-	}
+    @Then("^(\\w+) opens new Api Connector wizard$")
+    public void openNewApiConnectorWizard(String user) throws Throwable {
+        apiClientConnectorsPage.startWizard();
+    }
 
-	@Then("^(\\w+) opens the API connector \"([^\"]*)\" detail$")
-	public void openApiConnectorDetail(String userName, String connectorName) {
-		apiClientConnectorsPage.clickConnectorByTitle(connectorName);
-	}
+    @Then("^(\\w+) opens the API connector \"([^\"]*)\" detail$")
+    public void openApiConnectorDetail(String userName, String connectorName) {
+        apiClientConnectorsPage.clickConnectorByTitle(connectorName);
+    }
 
-	@Then("(\\w+) is presented with the new connector \"([^\"]*)\"$")
-	public void checkNewConnectorIsPresent(String user, String connectorName) throws Throwable {
-		Assert.assertTrue("Connector [" + connectorName + "] is not listed.", apiClientConnectorsPage.isConnectorPresent(connectorName));
-	}
+    @Then("(\\w+) is presented with the new connector \"([^\"]*)\"$")
+    public void checkNewConnectorIsPresent(String user, String connectorName) throws Throwable {
+        Assert.assertTrue("Connector [" + connectorName + "] is not listed.", apiClientConnectorsPage.isConnectorPresent(connectorName));
+    }
 
-	@Then("^(\\w+) is presented with a connectors list of size (\\d+)$")
-	public void checkConnectorsListSize(String userName, String listSize) {
-		Assert.assertTrue("The connectors list should be of size <" + listSize + ">.", apiClientConnectorsPage.isConnectorsListLongAs(Integer.parseInt(listSize)));
-	}
+    @Then("^(\\w+) is presented with a connectors list of size (\\d+)$")
+    public void checkConnectorsListSize(String userName, String listSize) {
+        Assert.assertTrue("The connectors list should be of size <" + listSize + ">.", apiClientConnectorsPage.isConnectorsListLongAs(Integer.parseInt(listSize)));
+    }
 
-	//***************************************************************************
-	//******************************* bulk steps ********************************
-	//***************************************************************************
+    //***************************************************************************
+    //******************************* bulk steps ********************************
+    //***************************************************************************
 
-	@Then("^(\\w+) creates new API connector \"([^\"]*)\"$")
-	public void createNewApiConnector(String user, String connectorName, DataTable properties) throws Throwable {
+    @Then("^(\\w+) creates new API connector \"([^\"]*)\"$")
+    public void createNewApiConnector(String user, String connectorName, DataTable properties) throws Throwable {
 
-		String securityAuthType = null;
+        String securityAuthType = null;
 
-		for (List<String> property : properties.raw()) {
-			switch (property.get(0)) {
-				case "security":
-					switch (property.get(1)) {
-						//| security | authType | OAuth 2.0 |
-						case "authType":
-							securityAuthType = property.get(2);
-							break;
-						default:
-					}
-					break;
+        for (List<String> property : properties.raw()) {
+            switch (property.get(0)) {
+                case "security":
+                    switch (property.get(1)) {
+                        //| security | authType | OAuth 2.0 |
+                        case "authType":
+                            securityAuthType = property.get(2);
+                            break;
+                        default:
+                    }
+                    break;
 
-				default:
-			}
-		}
+                default:
+            }
+        }
 
-		CommonSteps commonSteps = new CommonSteps();
-		commonSteps.navigateTo(user, "Customizations");
-		commonSteps.validatePage(user, "Customizations");
-		commonSteps.clickOnLink("API Client Connectors");
-		commonSteps.validatePage(user, "API Client Connectors");
+        CommonSteps commonSteps = new CommonSteps();
+        commonSteps.navigateTo(user, "Customizations");
+        commonSteps.validatePage(user, "Customizations");
+        commonSteps.clickOnLink("API Client Connectors");
+        commonSteps.validatePage(user, "API Client Connectors");
 
-		openNewApiConnectorWizard(user);
+        openNewApiConnectorWizard(user);
 
-		WizardSteps wizardSteps = new WizardSteps();
+        WizardSteps wizardSteps = new WizardSteps();
 
-		List<String> row = asList("file", "swagger_files/api_client_connector/petstore_swagger.json");
-		List<List<String>> dataTable = new ArrayList<>();
-		dataTable.add(row);
-		wizardSteps.uploadSwaggerFile(user, DataTable.create(dataTable));
+        List<String> row = asList("file", "swagger_files/api_client_connector/petstore_swagger.json");
+        List<List<String>> dataTable = new ArrayList<>();
+        dataTable.add(row);
+        wizardSteps.uploadSwaggerFile(user, DataTable.create(dataTable));
 
-		wizardSteps.navigateToNextWizardStep(user, "Review Swagger Action");
-		wizardSteps.navigateToNextWizardStep(user, "Security");
+        wizardSteps.navigateToNextWizardStep(user, "Review Swagger Action");
+        wizardSteps.navigateToNextWizardStep(user, "Security");
 
-		wizardSteps.setUpSecurityBy(user, securityAuthType);
-		wizardSteps.navigateToNextWizardStep(user, "General Connector Info");
-		wizardSteps.finishNewConnectorWizard(user);
-		checkNewConnectorIsPresent(user, connectorName);
-	}
+        wizardSteps.setUpSecurityBy(user, securityAuthType);
+        wizardSteps.navigateToNextWizardStep(user, "General Connector Info");
+        wizardSteps.finishNewConnectorWizard(user);
+        checkNewConnectorIsPresent(user, connectorName);
+    }
 }
--- ui-tests/src/test/java/io/syndesis/qe/steps/customizations/connectors/wizard/WizardSteps.java
+++ ui-tests/src/test/java/io/syndesis/qe/steps/customizations/connectors/wizard/WizardSteps.java
@@ -15,48 +15,48 @@ import io.syndesis.qe.pages.customizations.connectors.wizard.steps.UploadSwagger
 
 public class WizardSteps {
 
-	private ApiClientConnectorWizard wizard = new ApiClientConnectorWizard();
-	private UploadSwagger uploadSwaggerWizardStep = new UploadSwagger();
-	private Security securityWizardStep = new Security();
+    private ApiClientConnectorWizard wizard = new ApiClientConnectorWizard();
+    private UploadSwagger uploadSwaggerWizardStep = new UploadSwagger();
+    private Security securityWizardStep = new Security();
 
-	@And("^(\\w+) navigates to the next Api Connector wizard step \"([^\"]*)\"$")
-	public void navigateToNextWizardStep(String user, String step) {
-		wizard.nextStep();
+    @And("^(\\w+) navigates to the next Api Connector wizard step \"([^\"]*)\"$")
+    public void navigateToNextWizardStep(String user, String step) {
+        wizard.nextStep();
 
-		//validation ensures that multiple clicking the Next button doesn't click on the same button multiple times before the next step loads and displays
-		wizard.getCurrentStep().validate();
-	}
+        //validation ensures that multiple clicking the Next button doesn't click on the same button multiple times before the next step loads and displays
+        wizard.getCurrentStep().validate();
+    }
 
-	@And("^(\\w+) creates new connector$")
-	public void finishNewConnectorWizard(String user) {
-		wizard.nextStep();
-	}
+    @And("^(\\w+) creates new connector$")
+    public void finishNewConnectorWizard(String user) {
+        wizard.nextStep();
+    }
 
-	@Then("^(\\w+) uploads swagger file$")
-	public void uploadSwaggerFile(String user, DataTable fileParams) throws Throwable {
-		uploadSwaggerWizardStep.validate();
-		List<List<String>> dataRows = fileParams.cells(0);
-		List<String> sourceTypes = new ArrayList<String>();
-		List<String> urls = new ArrayList<String>();
+    @Then("^(\\w+) uploads swagger file$")
+    public void uploadSwaggerFile(String user, DataTable fileParams) throws Throwable {
+        uploadSwaggerWizardStep.validate();
+        List<List<String>> dataRows = fileParams.cells(0);
+        List<String> sourceTypes = new ArrayList<String>();
+        List<String> urls = new ArrayList<String>();
 
-		for (List<String> row : dataRows) {
-			sourceTypes.add(row.get(0));
-			urls.add(row.get(1));
-		}
+        for (List<String> row : dataRows) {
+            sourceTypes.add(row.get(0));
+            urls.add(row.get(1));
+        }
 
-		uploadSwaggerWizardStep.upload(sourceTypes.get(0), urls.get(0));
-	}
+        uploadSwaggerWizardStep.upload(sourceTypes.get(0), urls.get(0));
+    }
 
-	@Then("(\\w+) sets up security by \"([^\"]*)\"$")
-	public void setUpSecurityBy(String user, String authType) throws Throwable {
-		securityWizardStep.validate();
-		//String authType, String authenticationUrl, String authorizationUrl, String scopes
-		switch (authType) {
-			case "OAuth 2.0":
-				securityWizardStep.setUpOAuth2Security(TestConfiguration.syndesisUrl() + TestConfiguration.syndesisCallbackUrlSuffix());
-				break;
-			default:
-				Assert.fail("The Auth type < " + authType + "> is not implemented by the test.");
-		}
-	}
+    @Then("(\\w+) sets up security by \"([^\"]*)\"$")
+    public void setUpSecurityBy(String user, String authType) throws Throwable {
+        securityWizardStep.validate();
+        //String authType, String authenticationUrl, String authorizationUrl, String scopes
+        switch (authType) {
+            case "OAuth 2.0":
+                securityWizardStep.setUpOAuth2Security(TestConfiguration.syndesisUrl() + TestConfiguration.syndesisCallbackUrlSuffix());
+                break;
+            default:
+                Assert.fail("The Auth type < " + authType + "> is not implemented by the test.");
+        }
+    }
 }
