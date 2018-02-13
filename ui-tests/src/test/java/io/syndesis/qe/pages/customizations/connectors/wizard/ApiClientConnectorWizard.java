d8c5d61a634cd99290c84be1f2177207f06de080
Martin Stepanek <mastepan@localhost.localdomain>
2018-02-13 13:21:30 +0100

    [ui-tests] Refactor api connector

--- ui-tests/src/test/java/io/syndesis/qe/pages/customizations/connectors/wizard/ApiClientConnectorWizard.java
+++ ui-tests/src/test/java/io/syndesis/qe/pages/customizations/connectors/wizard/ApiClientConnectorWizard.java
@@ -3,38 +3,35 @@ package io.syndesis.qe.pages.customizations.connectors.wizard;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.$;
 
-import static java.util.Arrays.asList;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriverException;
 
 import com.codeborne.selenide.SelenideElement;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-
-import io.syndesis.qe.pages.SyndesisPageObject;
 import io.syndesis.qe.pages.WizardPageObject;
+import io.syndesis.qe.pages.customizations.connectors.wizard.steps.GeneralConnectorInfo;
+import io.syndesis.qe.pages.customizations.connectors.wizard.steps.ReviewSwaggerActions;
+import io.syndesis.qe.pages.customizations.connectors.wizard.steps.Security;
+import io.syndesis.qe.pages.customizations.connectors.wizard.steps.UploadSwagger;
 import io.syndesis.qe.pages.interfaces.wizard.WizardStep;
 
 public class ApiClientConnectorWizard extends WizardPageObject {
 
-    private static final class Element {
-        public static final By ROOT = By.cssSelector("syndesis-api-connector-create");
-    }
 
-    public ApiClientConnectorWizard() {
-        setSteps(new WizardStep[] {new UploadSwagger(), new ReviewSwaggerActions(), new Security(), new GeneralConnectorInfo()});
-    }
+	private static final class Element {
+		public static final By ROOT = By.cssSelector("syndesis-api-connectors-create");
+	}
 
-    @Override
-    public SelenideElement getRootElement() {
-        return $(Element.ROOT).should(exist);
-    }
+	public ApiClientConnectorWizard() {
+		setSteps(new WizardStep[] {new UploadSwagger(), new ReviewSwaggerActions(), new Security(), new GeneralConnectorInfo()});
+	}
 
-    @Override
-    public boolean validate() {
-        return getRootElement().exists();
-    }
+	@Override
+	public SelenideElement getRootElement() {
+		return $(Element.ROOT).should(exist);
+	}
+
+	@Override
+	public boolean validate() {
+		return getRootElement().exists();
+	}
 }
