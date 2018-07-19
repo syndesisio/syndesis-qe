1c09b8f5138b78479c4164f207815e41caadb2f0
mcada <mcada@redhat.com>
2018-07-19 14:26:48 +0200

    [ui][fix] fixing api connector tests

--- ui-tests/src/test/java/io/syndesis/qe/steps/customizations/connectors/detail/DetailSteps.java
+++ ui-tests/src/test/java/io/syndesis/qe/steps/customizations/connectors/detail/DetailSteps.java
@@ -1,28 +1,27 @@
 package io.syndesis.qe.steps.customizations.connectors.detail;
 
+import com.codeborne.selenide.Condition;
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Then;
 import io.syndesis.qe.pages.customizations.connectors.detail.ApiClientConnectorDetail;
-import org.assertj.core.api.Assertions;
+import org.openqa.selenium.By;
 
 import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DetailSteps {
 
     private ApiClientConnectorDetail connectorDetailPage = new ApiClientConnectorDetail();
 
-    @Then("^edit property")
+    @Then("^validate connector detail values")
     public void editProperty(DataTable dataTable) {
-        for (List<String> data : dataTable.raw()) {
-            String propertyName = data.get(0);
-            String propertyValue = data.get(1);
-            String id = data.get(2);
-
-            connectorDetailPage.getTextToEditElement(propertyName).click();
-            connectorDetailPage.getTextEditor(id).setValue(propertyValue);
-            connectorDetailPage.getEditablePropertyLabel(propertyName).click();
-            //check value has been set
-            Assertions.assertThat(connectorDetailPage.getTextToEditElement(propertyName).text()).isEqualToIgnoringCase(propertyValue);
+        for (List<String> dataRow : dataTable.raw()) {
+            String elementId = dataRow.get(0);
+            String expectedText = dataRow.get(1);
+            assertThat($(By.id(elementId)).shouldBe(Condition.visible).getAttribute("value"))
+                    .containsIgnoringCase(expectedText);
         }
     }
 }
