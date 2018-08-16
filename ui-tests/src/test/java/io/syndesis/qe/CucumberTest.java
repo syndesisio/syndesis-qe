6791a67428383cbd3846954f70b98160019c435e
mcada <mcada@redhat.com>
2018-08-16 14:20:55 +0200

    [ui] disable manual tests

--- ui-tests/src/test/java/io/syndesis/qe/CucumberTest.java
+++ ui-tests/src/test/java/io/syndesis/qe/CucumberTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "classpath:features",
-        tags = "not @wip",
+        tags = "not @wip, @manual, @deprecated",
         format = {"pretty", "html:target/cucumber/cucumber-html", "junit:target/cucumber/cucumber-junit.xml", "json:target/cucumber/cucumber-report.json"}
 )
 public class CucumberTest extends TestSuiteParent {
