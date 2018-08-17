ff148d9f176412d5cd05b2a5dc4ae93acc4e6528
mcada <mcada@redhat.com>
2018-08-17 14:29:43 +0200

    [ui][fix] Add dynamic wait when checking for existing extension

--- ui-tests/src/test/java/io/syndesis/qe/pages/customizations/extensions/TechExtensionsListComponent.java
+++ ui-tests/src/test/java/io/syndesis/qe/pages/customizations/extensions/TechExtensionsListComponent.java
@@ -1,17 +1,21 @@
 package io.syndesis.qe.pages.customizations.extensions;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.syndesis.qe.pages.SyndesisPageObject;
+import io.syndesis.qe.wait.OpenShiftWaitUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.By;
+
+import java.util.concurrent.TimeoutException;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-
-import org.openqa.selenium.By;
-
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
-
-import io.syndesis.qe.pages.SyndesisPageObject;
-import lombok.extern.slf4j.Slf4j;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @Slf4j
 public class TechExtensionsListComponent extends SyndesisPageObject {
@@ -37,13 +41,18 @@ public class TechExtensionsListComponent extends SyndesisPageObject {
     public SelenideElement getExtensionItem(String name) {
         $(Element.LIST_WRAPPER).shouldBe(visible);
 
+        try {
+            OpenShiftWaitUtils.waitFor(() ->
+                    $$(Element.ITEM).stream()
+                    .filter(item -> item.find(Element.ITEM_TITLE).getText().equals(name)).count() > 0, 15*1000);
+        } catch (InterruptedException | TimeoutException e) {
+            return null;
+        }
+
         ElementsCollection items = $$(Element.ITEM);
-
-        SelenideElement resultItem = items.stream()
+        return items.stream()
                 .filter(item -> item.find(Element.ITEM_TITLE).getText().equals(name))
-                .findAny().orElse(null);
-
-        return resultItem;
+                .findFirst().get();
     }
 
     public boolean isExtensionPresent(String name) {
@@ -59,6 +68,7 @@ public class TechExtensionsListComponent extends SyndesisPageObject {
     public void chooseActionOnExtension(String name, String action) {
         SelenideElement extension = this.getExtensionItem(name);
         SelenideElement actionButton = extension.findAll(By.tagName("button")).filter(exactText(action)).first();
+        assertThat(actionButton).isNotNull();
         actionButton.shouldBe(visible).click();
     }
 }
