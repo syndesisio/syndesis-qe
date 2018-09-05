f9e98c7259cdbd8098961d44f543469264a2689e
Andrej Vano <avano@redhat.com>
2018-09-05 11:27:48 +0200

    [Ignore] Ignore tests with @ignore tag

--- rest-tests/src/test/java/io/syndesis/qe/CucumberTestsRunner.java
+++ rest-tests/src/test/java/io/syndesis/qe/CucumberTestsRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "classpath:features", tags = {"not @integrations-lifecycle", "not @integrations-lifecycle-long", "not @manual", "not @wip"},
+    features = "classpath:features", tags = {"not @integrations-lifecycle", "not @integrations-lifecycle-long", "not @manual", "not @wip", "not @ignore"},
     plugin = {"pretty", "html:target/cucumber/cucumber-html", "junit:target/cucumber/cucumber-junit.xml", "json:target/cucumber/cucumber-report.json"})
 public class CucumberTestsRunner extends TestSuiteParent {
 
