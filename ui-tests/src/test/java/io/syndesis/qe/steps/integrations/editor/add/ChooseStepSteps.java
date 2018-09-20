723666c931eaaecc66fa8ce0fb51ddf4fde064c9
mcada <mcada@redhat.com>
2018-09-20 13:46:36 +0200

    [ui][fix] add dynamic wait for data mapper

--- ui-tests/src/test/java/io/syndesis/qe/steps/integrations/editor/add/ChooseStepSteps.java
+++ ui-tests/src/test/java/io/syndesis/qe/steps/integrations/editor/add/ChooseStepSteps.java
@@ -2,7 +2,12 @@ package io.syndesis.qe.steps.integrations.editor.add;
 
 import cucumber.api.java.en.When;
 import io.syndesis.qe.pages.integrations.editor.add.ChooseStep;
+import io.syndesis.qe.wait.OpenShiftWaitUtils;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeoutException;
+
+import static org.assertj.core.api.Assertions.fail;
 
 @Slf4j
 public class ChooseStepSteps {
@@ -12,6 +17,11 @@ public class ChooseStepSteps {
     @When("^select \"([^\"]*)\" integration step$")
     public void chooseStep(String stepName) {
         log.info("Adding {} step to integration", stepName);
+        try {
+            OpenShiftWaitUtils.waitFor(() -> chooseStep.validate(), 1000 * 30L);
+        } catch (TimeoutException | InterruptedException e) {
+            fail("Choose step page was not loaded in 30s!", e);
+        }
         chooseStep.chooseStep(stepName);
     }
 
