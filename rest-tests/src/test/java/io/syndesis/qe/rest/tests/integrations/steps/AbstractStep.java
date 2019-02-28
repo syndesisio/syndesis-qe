71ac0d2361c126c25c803c9609204b911e58eb9a
Andrej Vano <avano@redhat.com>
2019-02-28 14:59:05 +0100

    [FUSEQE-3296] Refactor HTTP

---
+++ rest-tests/src/test/java/io/syndesis/qe/rest/tests/integrations/steps/AbstractStep.java
@@ -0,0 +1,172 @@
+package io.syndesis.qe.rest.tests.integrations.steps;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import io.syndesis.common.model.DataShape;
+import io.syndesis.common.model.DataShapeKinds;
+import io.syndesis.common.model.action.Action;
+import io.syndesis.common.model.action.ActionDescriptor;
+import io.syndesis.common.model.action.ConnectorAction;
+import io.syndesis.common.model.action.ConnectorDescriptor;
+import io.syndesis.common.model.connection.Connection;
+import io.syndesis.common.model.connection.Connector;
+import io.syndesis.common.model.integration.Step;
+import io.syndesis.common.model.integration.StepKind;
+import io.syndesis.common.util.Json;
+import io.syndesis.qe.bdd.entities.StepDefinition;
+import io.syndesis.qe.bdd.storage.StepsStorage;
+import io.syndesis.qe.endpoints.ConnectionsActionsEndpoint;
+import io.syndesis.qe.endpoints.ConnectionsEndpoint;
+import io.syndesis.qe.endpoints.ConnectorsEndpoint;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Mar 8, 2018 Red Hat
+ *
+ * @author tplevko@redhat.com
+ */
+@Slf4j
+public abstract class AbstractStep {
+    public enum StepProperty {
+        KIND,
+        STEP_ID,
+        STEP_NAME,
+        CONNECTOR_ID,
+        CONNECTION_ID,
+        ACTION,
+        PROPERTIES
+    }
+
+    @Autowired
+    @Getter
+    private StepsStorage steps;
+    @Autowired
+    private ConnectionsEndpoint connectionsEndpoint;
+    @Autowired
+    private ConnectorsEndpoint connectorsEndpoint;
+
+    private Map<StepProperty, Object> properties = new HashMap<>();
+
+    public void addProperty(StepProperty key, Object value) {
+        properties.put(key, value);
+    }
+
+    public void createStep() {
+        // Some steps do not have connector / connection
+        Connector connector = properties.get(StepProperty.CONNECTOR_ID) == null ? null : connectorsEndpoint.get((String) properties.get(StepProperty.CONNECTOR_ID));
+        Connection connection = properties.get(StepProperty.CONNECTION_ID) == null ? null : connectionsEndpoint.get((String) properties.get(StepProperty.CONNECTION_ID));
+        Action action;
+        // If the action is not String, then we already have action object, so just use it
+        if (properties.get(StepProperty.ACTION) != null && !(properties.get(StepProperty.ACTION) instanceof String)) {
+            action = (Action) properties.get(StepProperty.ACTION);
+        } else {
+            action = properties.get(StepProperty.ACTION) == null ? null : findConnectorAction(connector, (String) properties.get(StepProperty.ACTION));
+            if (action != null) {
+                action = generateStepAction(action, getConnectorDescriptor(action, (Map) properties.get(StepProperty.PROPERTIES),
+                        (String) properties.get(StepProperty.CONNECTION_ID)));
+            }
+        }
+
+        final Step.Builder stepBuilder = new Step.Builder();
+        stepBuilder.stepKind(properties.get(StepProperty.KIND) == null ? StepKind.endpoint : (StepKind) properties.get(StepProperty.KIND));
+        stepBuilder.id(properties.get(StepProperty.STEP_ID) == null ? UUID.randomUUID().toString() : (String) properties.get(StepProperty.STEP_ID));
+        stepBuilder.name(properties.get(StepProperty.STEP_NAME) == null ? UUID.randomUUID().toString() : (String) properties.get(StepProperty.STEP_NAME));
+        if (connection != null) {
+            stepBuilder.connection(connection);
+        }
+        if (action != null) {
+            stepBuilder.action(action);
+        }
+        steps.getStepDefinitions().add(new StepDefinition(stepBuilder.build()));
+        if (properties.get(StepProperty.PROPERTIES) != null) {
+            stepBuilder.configuredProperties((Map) properties.get(StepProperty.PROPERTIES));
+        }
+        properties.clear();
+    }
+
+    public Action findConnectorAction(Connector connector, String connectorPrefix) {
+        Optional<ConnectorAction> action;
+        action = connector.getActions()
+                .stream()
+                .filter(a -> a.getId().get().contains(connectorPrefix))
+                .findFirst();
+
+        if (!action.isPresent()) {
+            action = connector.getActions()
+                    .stream()
+                    .filter(a -> a.getDescriptor().getCamelConnectorPrefix().contains(connectorPrefix))
+                    .findFirst();
+        }
+
+        return action.get();
+    }
+
+    public ConnectorDescriptor getConnectorDescriptor(Action action, Map properties, String connectionId) {
+        ConnectionsActionsEndpoint conActEndpoint = new ConnectionsActionsEndpoint(connectionId);
+        return conActEndpoint.postParamsAction(action.getId().get(), properties);
+    }
+
+    //Small hack -> the Action doesn't provide setters for input/output data shape
+    public Action generateStepAction(Action action, ActionDescriptor connectorDescriptor) {
+        ObjectMapper mapper = new ObjectMapper().registerModules(new Jdk8Module());
+        Action ts = null;
+        try {
+            JSONObject json = new JSONObject(mapper.writeValueAsString(action));
+            JSONObject inputDataType = new JSONObject(mapper.writeValueAsString(connectorDescriptor.getInputDataShape().get()));
+            JSONObject outputDataType = new JSONObject(mapper.writeValueAsString(connectorDescriptor.getOutputDataShape().get()));
+            JSONArray propertyDefinitionSteps = new JSONArray(mapper.writeValueAsString(connectorDescriptor.getPropertyDefinitionSteps()));
+
+            json.getJSONObject("descriptor").put("inputDataShape", inputDataType);
+            json.getJSONObject("descriptor").put("outputDataShape", outputDataType);
+            json.getJSONObject("descriptor").put("propertyDefinitionSteps", propertyDefinitionSteps);
+
+            ts = Json.reader().forType(Action.class).readValue(json.toString());
+        } catch (IOException ex) {
+            log.error("Error: " + ex);
+        }
+        return ts;
+    }
+
+    /**
+     * Sets the custom datashape to the action object.
+     * @param action action
+     * @param connectorDescriptor action's connector descriptor to fill other values
+     * @param direction "in" for InputDataShape, "out" for OutputDataShape
+     * @param kind {@link DataShapeKinds} value
+     * @param datashape Datashape specification
+     * @return action object with datashapes
+     */
+    public Action withCustomDatashape(Action action, ConnectorDescriptor connectorDescriptor, String direction, DataShapeKinds kind, String datashape) {
+        // This will set datashapes and property definitions from the connectorDescriptor
+        Action a = generateStepAction(action, connectorDescriptor);
+        ObjectMapper mapper = new ObjectMapper().registerModules(new Jdk8Module());
+        try {
+            JSONObject json = new JSONObject(mapper.writeValueAsString(a));
+            DataShape ds = new DataShape.Builder()
+                    .name(kind.toString())
+                    .description(kind.toString())
+                    .kind(kind)
+                    .specification(datashape)
+                    .build();
+
+            json.getJSONObject("descriptor").put("in".equals(direction) ? "inputDataShape" : "outputDataShape", new JSONObject(mapper.writeValueAsString(ds)));
+
+            a = Json.reader().forType(Action.class).readValue(json.toString());
+        } catch (IOException ex) {
+            log.error("Error: " + ex);
+        }
+        return a;
+    }
+}
--- rest-tests/src/test/java/io/syndesis/qe/rest/tests/integrations/steps/HTTPSteps.java
+++ rest-tests/src/test/java/io/syndesis/qe/rest/tests/integrations/steps/HTTPSteps.java
@@ -1,39 +1,18 @@
 package io.syndesis.qe.rest.tests.integrations.steps;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import cucumber.api.java.en.Given;
-import io.syndesis.common.model.DataShapeKinds;
-import io.syndesis.common.model.action.Action;
-import io.syndesis.common.model.action.ConnectorDescriptor;
-import io.syndesis.common.model.connection.Connection;
-import io.syndesis.common.model.connection.Connector;
-import io.syndesis.common.model.integration.Step;
-import io.syndesis.common.model.integration.StepKind;
-import io.syndesis.qe.bdd.entities.StepDefinition;
-import io.syndesis.qe.bdd.storage.StepsStorage;
-import io.syndesis.qe.endpoints.ConnectionsEndpoint;
-import io.syndesis.qe.endpoints.ConnectorsEndpoint;
 import io.syndesis.qe.rest.tests.util.RestTestsUtils;
 import io.syndesis.qe.utils.TestUtils;
 
 public class HTTPSteps extends AbstractStep {
-    @Autowired
-    private StepsStorage steps;
-    @Autowired
-    private ConnectionsEndpoint connectionsEndpoint;
-    @Autowired
-    private ConnectorsEndpoint connectorsEndpoint;
 
-    private void createStep(String method, String path, long period, String timeunit, String datashape) {
-        final Connector httpConnector = connectorsEndpoint.get(RestTestsUtils.Connector.HTTP.getId());
-        final Connection httpConnection = connectionsEndpoint.get(RestTestsUtils.Connection.HTTP.getId());
-        final String action = period == -1 ? "http4-invoke-url" : "http4-periodic-invoke-url";
-        Action httpAction = TestUtils.findConnectorAction(httpConnector, action);
+    private void createStep(String method, String path, long period, String timeunit) {
+        super.addProperty(StepProperty.CONNECTOR_ID, RestTestsUtils.Connector.HTTP.getId());
+        super.addProperty(StepProperty.CONNECTION_ID, RestTestsUtils.Connection.HTTP.getId());
+        super.addProperty(StepProperty.ACTION, period == -1 ? "http4-invoke-url" : "http4-periodic-invoke-url");
         final Map<String, String> properties = TestUtils.map(
                 "path", path,
                 "httpMethod", method
@@ -42,40 +21,22 @@ public class HTTPSteps extends AbstractStep {
         if (period != -1) {
             properties.put("schedulerExpression", TimeUnit.MILLISECONDS.convert(period, TimeUnit.valueOf(timeunit)) + "");
         }
-
-        if (datashape != null) {
-            final ConnectorDescriptor cd = getConnectorDescriptor(httpAction, properties, RestTestsUtils.Connection.HTTP.getId());
-            httpAction = withCustomDatashape(
-                    httpAction,
-                    cd,
-                    "out",
-                    DataShapeKinds.XML_INSTANCE,
-                    datashape);
-        }
-
-        final Step httpStep = new Step.Builder()
-                .stepKind(StepKind.endpoint)
-                .id(UUID.randomUUID().toString())
-                .connection(httpConnection)
-                .action(httpAction)
-                .configuredProperties(properties)
-                .build();
-
-        steps.getStepDefinitions().add(new StepDefinition(httpStep));
+        super.addProperty(StepProperty.PROPERTIES, properties);
+        super.createStep();
     }
 
     @Given("^create HTTP \"([^\"]*)\" step with period \"([^\"]*)\" \"([^\"]*)\"$")
     public void createHTTPStepWithPeriod(String method, long period, String timeunit) {
-        createStep(method, "/", period, timeunit, null);
+        createStep(method, "/", period, timeunit);
     }
 
     @Given("^create HTTP \"([^\"]*)\" step with path \"([^\"]*)\" and period \"([^\"]*)\" \"([^\"]*)\"$")
     public void createHTTPStepWithPeriodAndPath(String method, String path, long period, String timeunit) {
-        createStep(method, path, period, timeunit, null);
+        createStep(method, path, period, timeunit);
     }
 
     @Given("^create HTTP \"([^\"]*)\" step$")
     public void createHTTPStep(String method) {
-        createStep(method, "/", -1, null, null);
+        createStep(method, "/", -1, null);
     }
 }
