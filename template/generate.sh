dccce1b11b772da3a213a6b115d883c46fa1293a
Andrej Vano <avano@redhat.com>
2019-03-27 14:37:50 +0100

    [Generator] Fix for current version

--- template/generate.sh
+++ template/generate.sh
@@ -2,23 +2,25 @@
 BASE_DIR=$(dirname "$(readlink -f "$0")")
 
 # Variables used:
-#SERVER          - server image
-#META            - meta image
-#UI              - ui image
-#S2I             - s2i image
-#OPERATOR        - operator
-#TAG             - tag which is expected by the operator
-#OAUTH_PROXY     - oauth proxy image, defaults to "registry.access.redhat.com/openshift3/oauth-proxy:v3.10.45"
-#OAUTH_PROXY_TAG - oauth proxy image stream tag, defaults to "v1.1.0"
-#PROMETHEUS      - prometheus image, defaults to "registry.access.redhat.com/openshift3/prometheus:v3.9.25"
-#PROMETHEUS_TAG  - prometheus image stream tag, defaults to "v2.1.0"
+#SERVER                - server image
+#META                  - meta image
+#UI                    - ui image
+#S2I                   - s2i image
+#OPERATOR              - operator
+#TAG                   - tag which is expected by the operator
+#OAUTH_PROXY           - oauth proxy image, defaults to "registry.access.redhat.com/openshift3/oauth-proxy:v3.10.45"
+#OAUTH_PROXY_TAG       - oauth proxy image stream tag, defaults to "v1.1.0"
+#PROMETHEUS            - prometheus image, defaults to "registry.access.redhat.com/openshift3/prometheus:v3.9.25"
+#PROMETHEUS_TAG        - prometheus image stream tag, defaults to "v2.1.0"
+#POSTGRES_EXPORTER     - postgres exporter image
+#POSTGRES_EXPORTER_TAG - postgres exporter image stream tag, default to "v0.4.7"
 
 # Generated template is located in /tmp/prod.yml and the content it printed when the script is done
 
 # Script can be also used as ./generate.sh | xclip -selection c
 # to copy the template to clipboard
 
-VARS="SERVER META UI S2I OPERATOR OAUTH_PROXY PROMETHEUS TAG OAUTH_PROXY_TAG PROMETHEUS_TAG"
+VARS="SERVER META UI S2I OPERATOR OAUTH_PROXY PROMETHEUS TAG OAUTH_PROXY_TAG PROMETHEUS_TAG POSTGRES_EXPORTER POSTGRES_EXPORTER_TAG"
 
 # If the properties are defined, don't source
 [[ ! "z${OPERATOR}" == "z" ]] || source "${BASE_DIR}"/vars
