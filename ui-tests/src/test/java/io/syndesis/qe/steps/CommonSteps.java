13662720f3c1f90f6347ae1e5d5a31e0cc7da807
Michael Cada <mcada@redhat.com>
2019-04-11 08:57:48 +0000

    [ui][fix] stabilize support page and data buckets tests

--- ui-tests/src/test/java/io/syndesis/qe/steps/CommonSteps.java
+++ ui-tests/src/test/java/io/syndesis/qe/steps/CommonSteps.java
@@ -43,8 +43,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.cucumber.datatable.DataTable;
-import io.fabric8.kubernetes.api.model.EnvVar;
-import io.fabric8.openshift.api.model.DeploymentConfig;
 import io.syndesis.qe.CustomWebDriverProvider;
 import io.syndesis.qe.TestConfiguration;
 import io.syndesis.qe.accounts.Account;
@@ -247,6 +245,15 @@ public class CommonSteps {
             nameConnectionSteps.setConnectionDescription(connectionDescription);
 
             clickOnButton("Create");
+
+            if (syndesisRootPage.getCurrentUrl().contains("connections/create/review")) {
+                clickOnButton("Create");
+                try {
+                    OpenShiftWaitUtils.waitFor(() -> !syndesisRootPage.getCurrentUrl().contains("connections/create/review"), 15 * 1000L);
+                } catch (TimeoutException | InterruptedException e) {
+                    fail("Unable to create a connection - create button does nothing.");
+                }
+            }
         }
     }
 
