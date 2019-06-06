745fbeceda67a723a8d9b7c2866507b3559ea369
mkralik3 <mkralik@redhat.com>
2019-06-06 10:54:23 +0200

    [fix] - fixed creation of zip file  * added check whether the zip file was created successfully

--- utilities/src/main/java/io/syndesis/qe/endpoints/publicendpoint/IntegrationsPublicEndpoint.java
+++ utilities/src/main/java/io/syndesis/qe/endpoints/publicendpoint/IntegrationsPublicEndpoint.java
@@ -25,9 +25,13 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipFile;
 
 import static org.assertj.core.api.Assertions.fail;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 @Lazy
 public class IntegrationsPublicEndpoint extends PublicEndpoint {
@@ -48,7 +52,7 @@ public class IntegrationsPublicEndpoint extends PublicEndpoint {
         MultipartFormDataOutput mdo = new MultipartFormDataOutput();
         try {
             mdo.addFormData("data", new FileInputStream(Paths.get("./target/" + name).toAbsolutePath().normalize().toFile()),
-                    MediaType.APPLICATION_OCTET_STREAM_TYPE);
+                MediaType.APPLICATION_OCTET_STREAM_TYPE);
             mdo.addFormData("environment", tag, MediaType.TEXT_PLAIN_TYPE);
         } catch (FileNotFoundException e) {
             fail("IO Exception during loading ZIP file.", e);
@@ -181,15 +185,41 @@ public class IntegrationsPublicEndpoint extends PublicEndpoint {
      */
     private void exportZip(InputStream is, String name) {
         try {
-            byte[] buffer = new byte[is.available()];
-            is.read(buffer);
             File targetFile = Paths.get("./target/" + name).toAbsolutePath().normalize().toFile();
             OutputStream outStream = new FileOutputStream(targetFile);
-            outStream.write(buffer);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                outStream.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            outStream.flush();
             outStream.close();
+            checkThatZipFileIsValid(Paths.get("./target/" + name).toAbsolutePath().normalize().toFile());
         } catch (IOException e) {
             e.printStackTrace();
             fail("IO Exception during exporting ZIP file.", e);
+        }
+    }
+
+    /**
+     * Check that imported zip file is valid
+     */
+    private void checkThatZipFileIsValid(File file) {
+        ZipFile zipfile = null;
+        try {
+            zipfile = new ZipFile(file);
+            log.debug("Size of the zipfile is: " + zipfile.size());
+        } catch (IOException e) {
+            fail("The zip file is not valid");
+        } finally {
+            try {
+                if (zipfile != null) {
+                    zipfile.close();
+                    zipfile = null;
+                }
+            } catch (IOException e) {
+            }
         }
     }
 }
