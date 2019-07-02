daa8fb15f945c05cea96f538d35001bdf9283327
Alice Rum <alice.rum@redhat.com>
2019-07-02 16:37:27 +0200

    [FUSEQE-4509] implementation of twitter DM testing
    These are new tests which test new feature - twitter direct messages

--- utilities/src/main/java/io/syndesis/qe/bdd/validation/TwValidationSteps.java
+++ utilities/src/main/java/io/syndesis/qe/bdd/validation/TwValidationSteps.java
@@ -1,10 +1,19 @@
 package io.syndesis.qe.bdd.validation;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
+import static org.assertj.core.api.Assertions.fail;
+
 import io.syndesis.qe.accounts.Account;
 import io.syndesis.qe.accounts.AccountsDirectory;
+import io.syndesis.qe.utils.TestUtils;
+
+import java.util.Optional;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import lombok.extern.slf4j.Slf4j;
+import twitter4j.DirectMessage;
+import twitter4j.DirectMessageList;
 import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -21,34 +30,110 @@ import twitter4j.conf.ConfigurationBuilder;
  */
 @Slf4j
 public class TwValidationSteps {
-    private final Twitter twitter;
+    private final Twitter twitterTalky;
+    private final Twitter twitterListener;
     private final AccountsDirectory accountsDirectory;
 
     public TwValidationSteps() {
         accountsDirectory = AccountsDirectory.getInstance();
-        final Account twitterTalky = accountsDirectory.getAccount(Account.Name.TWITTER_TALKY).get();
-        //twitter
-        final TwitterFactory factory = new TwitterFactory(new ConfigurationBuilder()
-                .setOAuthConsumerKey(twitterTalky.getProperty("consumerKey"))
-                .setOAuthConsumerSecret(twitterTalky.getProperty("consumerSecret"))
-                .setOAuthAccessToken(twitterTalky.getProperty("accessToken"))
-                .setOAuthAccessTokenSecret(twitterTalky.getProperty("accessTokenSecret"))
+        final Account twitterTalkyAccount = accountsDirectory.getAccount(Account.Name.TWITTER_TALKY).get();
+        //twitterTalky
+        final TwitterFactory factoryTalk = new TwitterFactory(new ConfigurationBuilder()
+                .setOAuthConsumerKey(twitterTalkyAccount.getProperty("consumerKey"))
+                .setOAuthConsumerSecret(twitterTalkyAccount.getProperty("consumerSecret"))
+                .setOAuthAccessToken(twitterTalkyAccount.getProperty("accessToken"))
+                .setOAuthAccessTokenSecret(twitterTalkyAccount.getProperty("accessTokenSecret"))
                 .build());
-        twitter = factory.getInstance();
+        this.twitterTalky = factoryTalk.getInstance();
+
+        final Account twitterListenerAccount = accountsDirectory.getAccount(Account.Name.TWITTER_LISTENER).get();
+        final TwitterFactory factoryListen = new TwitterFactory(new ConfigurationBuilder()
+            .setOAuthConsumerKey(twitterListenerAccount.getProperty("consumerKey"))
+            .setOAuthConsumerSecret(twitterListenerAccount.getProperty("consumerSecret"))
+            .setOAuthAccessToken(twitterListenerAccount.getProperty("accessToken"))
+            .setOAuthAccessTokenSecret(twitterListenerAccount.getProperty("accessTokenSecret"))
+            .build());
+        this.twitterListener = factoryListen.getInstance();
     }
 
     @Given("^clean all tweets in twitter_talky account")
     public void cleanupTwSf() throws TwitterException {
-        deleteAllTweets(twitter);
+        deleteAllTweets(twitterTalky);
     }
 
-    @Then("^tweet a message from twitter_talky to \"([^\"]*)\" with text \"([^\"]*)\"")
+    @When("^tweet a message from twitter_talky to \"([^\"]*)\" with text \"([^\"]*)\"")
     public void sendTweet(String toAcc, String tweet) throws TwitterException {
         final String message = tweet + " @" + accountsDirectory.getAccount(toAcc).get().getProperty("screenName");
         log.info("Sending a tweet from {}, to {} with message: {}", accountsDirectory.getAccount(Account.Name.TWITTER_TALKY)
                 .get().getProperty("screenName"), accountsDirectory.getAccount(toAcc).get().getProperty("screenName"), message);
-        twitter.updateStatus(message);
+        twitterTalky.updateStatus(message);
         log.info("Tweet submitted.");
+    }
+
+    @When("^send direct message from twitter_talky to \"([^\"]*)\" with text \"([^\"]*)\"$")
+    public void sendDirectMessage(String toAcc, String message) throws TwitterException {
+        Optional<Account> optionalAccount = accountsDirectory.getAccount(toAcc);
+        if (optionalAccount.isPresent()) {
+            String screenName = optionalAccount.get().getProperty("screenName");
+            log.info("Sending twitterTalky DM to user {} with text {}", screenName, message);
+            twitterTalky.sendDirectMessage(screenName, message);
+        } else {
+            fail("Could not find account {}", toAcc);
+        }
+    }
+
+    @Then("^check that account \"([^\"]*)\" has DM from user \"([^\"]*)\" with text \"([^\"]*)\"")
+    public void checkDirectMessage(String account, long senderId, String message) {
+        Twitter twitter = null;
+        if ("twittertalky".equals(account.toLowerCase().replaceAll(" ", ""))) {
+            twitter = this.twitterTalky;
+        } else if ("twitterlistener".equals(account.toLowerCase().replaceAll(" ", ""))) {
+            twitter = this.twitterListener;
+        } else {
+            fail("Wrond twitter account: " + account);
+        }
+
+        Twitter finalTwitter = twitter;
+        TestUtils.waitFor(() -> checkDirectMessage(finalTwitter, senderId, message),
+            10, 60,
+            "Could not find correct twitter DM");
+    }
+
+    private boolean checkDirectMessage(Twitter twitter, long senderId, String messageText) {
+        boolean result = false;
+
+        try {
+            for (DirectMessage message : twitter.getDirectMessages(100)) {
+                log.info("twitter DM from user {} with text {}", message.getSenderId(), message.getText());
+                if (messageText.equalsIgnoreCase(message.getText()) &&
+                    senderId == message.getSenderId()) {
+
+                    result = true;
+                }
+            }
+        } catch (TwitterException e) {
+            log.debug("Exception while twitter interaction", e);
+        }
+
+        return result;
+    }
+
+    @When("^delete all direct messages received by \"([^\"]*)\"$")
+    public void deleteAllDMs(String account) throws TwitterException {
+        if ("twittertalky".equals(account.toLowerCase().replaceAll(" ", ""))) {
+            deleteAllDMs(this.twitterTalky);
+        }
+
+        if ("twitterlistener".equals(account.toLowerCase().replaceAll(" ", ""))) {
+            deleteAllDMs(this.twitterListener);
+        }
+    }
+
+    private void deleteAllDMs(Twitter twitter) throws TwitterException {
+        DirectMessageList messageList = twitter.getDirectMessages(100);
+        for (DirectMessage directMessage : messageList) {
+            twitter.destroyDirectMessage(directMessage.getId());
+        }
     }
 
     private void deleteAllTweets(Twitter twitter) throws TwitterException {
