30964e135543cbfd3c55953bf75c3479d4faf9a9
Andrej Vano <avano@redhat.com>
2019-09-27 10:04:39 +0200

    [FUSEQE-5666] Add test for box download datamapper

--- utilities/src/main/java/io/syndesis/qe/bdd/validation/BoxValidationSteps.java
+++ utilities/src/main/java/io/syndesis/qe/bdd/validation/BoxValidationSteps.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Fail.fail;
 
 import io.syndesis.qe.utils.BoxUtils;
+import io.syndesis.qe.utils.DbUtils;
 import io.syndesis.qe.utils.JMSUtils;
 
 import org.apache.commons.io.FileUtils;
@@ -14,17 +15,22 @@ import com.box.sdk.BoxFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class BoxValidationSteps {
     @Autowired
     private BoxUtils boxUtils;
+
+    private DbUtils dbUtils = new DbUtils("postgresql");
 
     @Given("^remove all files from Box$")
     public void clearBox() {
@@ -60,6 +66,28 @@ public class BoxValidationSteps {
     public void verifyBoxResponse(String queueName, String text) {
         final String message = JMSUtils.getMessageText(JMSUtils.Destination.QUEUE, queueName);
         final int expectedMessageSize = text.getBytes().length;
-        assertThat(message).isEqualTo(String.format("{\"text\":\"%s-%s-%d\"}", text, BoxUtils.getFileId(), expectedMessageSize));
+        assertThat(message).isEqualTo(String.format("{\"text\":\"%s-%s-%d\"}", text, BoxUtils.getFileIds().get(0), expectedMessageSize));
+    }
+
+    @Then("^verify that all box messages were received from \"([^\"]*)\" queue:$")
+    public void verifyBoxMessages(String queueName, DataTable expectedMessages) {
+        List<String> messages = new ArrayList<>();
+        String message = JMSUtils.getMessageText(JMSUtils.Destination.QUEUE, queueName);
+        while (message != null) {
+            messages.add(message);
+            message = JMSUtils.getMessageText(JMSUtils.Destination.QUEUE, queueName);
+        }
+
+        assertThat(messages).size().isEqualTo(expectedMessages.asList().size());
+        assertThat(messages).contains(
+            expectedMessages.asList().toArray(new String[0])
+        );
+    }
+
+    @Given("^insert box file ids to box id table$")
+    public void insertIdsInTable() {
+        for (String fileId : BoxUtils.getFileIds()) {
+            dbUtils.executeSQLGetUpdateNumber("INSERT INTO BOX_IDS(id) VALUES(" + fileId + ")");
+        }
     }
 }
