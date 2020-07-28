5de4f9f2cd8a1bb020e28206e90f1ba42cb6c908
mkralik3 <mkralik@redhat.com>
2020-07-28 15:18:27 +0200

    [FUSEQE-9071] Dockerfile for UI tests

--- docker/rest/syndesisQe.sh
+++ docker/rest/syndesisQe.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "=============== Syndesis QE (rest) test suite (modified for usage in Delorean) ==============="
-TAGS="@smoke"
+echo "=============== Syndesis QE (rest) test suite ==============="
 echo "Environment variables:"
+echo "URL: ${URL}"
+echo "ADMIN_USERNAME: ${ADMIN_USERNAME}"
+echo "ADMIN_PASSWORD: ${ADMIN_PASSWORD}"
 echo "NAMESPACE: ${NAMESPACE}"
 echo "TAGS: ${TAGS}"
 echo "CREDENTIALS_URL: ${CREDENTIALS_URL}"
@@ -12,7 +14,7 @@ if [[ -z "${CREDENTIALS_URL}" ]]; then
 else
   if [[ ${CREDENTIALS_URL} == *"credentials.json" ]]; then
     echo "URL for credentials.json exist. That credentials will be used."
-    wget -N "${CREDENTIALS_URL}"
+    wget -N "${CREDENTIALS_URL}" --no-check-certificate
   else
     echo "The URL set in CREDENTIALS_URL doesn't contain credentials.json. The default credentials.json will be used. Tests that use 3rd party services will fail."
   fi
@@ -20,23 +22,23 @@ fi
 
 mvn clean test -P rest \
 		-Dcucumber.options="--tags '""${TAGS}""'" \
+		-Dsyndesis.config.openshift.url="${URL}" \
+		-Dsyndesis.config.admin.username="${ADMIN_USERNAME}" \
+		-Dsyndesis.config.admin.password="${ADMIN_PASSWORD}" \
 		-Dsyndesis.config.openshift.namespace="${NAMESPACE}" \
 		-Dsyndesis.config.openshift.namespace.lock=false \
 		-Dsyndesis.config.openshift.namespace.cleanup=false \
 		-Dmaven.surefire.debug="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xnoagent -Djava.compiler=NONE" \
-		-Dsyndesis.config.environment.delorean=true
+		-Dsyndesis.config.enableTestSupport=true
 
 STATUS=$?
 
 echo "Status code from mvn command is: $STATUS . The test results will be stored into /test-run-results folder"
 
-mkdir /test-run-results
-mv ./rest-tests/target/cucumber/cucumber-html \
-   ./rest-tests/target/cucumber/cucumber-junit.xml \
-   ./rest-tests/target/cucumber/cucumber-mail \
-   ./rest-tests/target/cucumber/cucumber-report.json \
-   ./rest-tests/log/* \
+[ -d "/test-run-results" ] && rm -rf /test-run-results/cucumber /test-run-results/log || mkdir /test-run-results
+
+mv ./rest-tests/target/cucumber \
+   ./rest-tests/log/ \
    /test-run-results
-cp /test-run-results/cucumber-junit.xml /test-run-results/junit.xml
 
 exit $STATUS
--- docker/ui/syndesisQe_ui.sh
+++ docker/ui/syndesisQe_ui.sh
@@ -1,47 +1,66 @@
 #!/bin/bash
 
-echo "=============== Syndesis QE (rest) test suite ==============="
+echo "=============== Syndesis QE (ui) test suite ==============="
 echo "Environment variables:"
 echo "URL: ${URL}"
 echo "ADMIN_USERNAME: ${ADMIN_USERNAME}"
 echo "ADMIN_PASSWORD: ${ADMIN_PASSWORD}"
+echo "UI_USERNAME: ${UI_USERNAME}"
+echo "UI_PASSWORD: ${UI_PASSWORD}"
 echo "NAMESPACE: ${NAMESPACE}"
 echo "TAGS: ${TAGS}"
 echo "CREDENTIALS_URL: ${CREDENTIALS_URL}"
+echo "USER: $(whoami)"
 
 if [[ -z "${CREDENTIALS_URL}" ]]; then
   echo "URL for credentials.json with 3rd party services credentials was not set in CREDENTIALS_URL env. Tests that use 3rd party services will fail."
 else
   if [[ ${CREDENTIALS_URL} == *"credentials.json" ]]; then
     echo "URL for credentials.json exist. That credentials will be used."
-    wget -N "${CREDENTIALS_URL}"
+    wget -N "${CREDENTIALS_URL}" --no-check-certificate
   else
     echo "The URL set in CREDENTIALS_URL doesn't contain credentials.json. The default credentials.json will be used. Tests that use 3rd party services will fail."
   fi
 fi
 
-mvn clean test -P rest \
+if [[ ! -z "${UI_URL}" ]] ; then
+  echo "The Syndesis UI URL was specify. Please make sure that you have correctly set UI IP in your /etc/hosts since the container uses hosts \
+        from the local machine where it is running!"
+  echo "UI_URL: ${UI_URL}"
+  echo "====== /etc/hosts: ${UI_IP} ======"
+  sudo cat /etc/hosts
+  echo "=================================="
+  UI_URL_CONFIG="-Dsyndesis.config.ui.url="${UI_URL}""
+else
+    echo "The Syndesis UI URL was not specify. The test suite suppose that the Syndesis UI URL is in default format (when the user doesn't \
+          specify URL during the Syndesis install, e.g. https://syndesis-<namespace>.apps.<cluster>). The OAuth tests will not work!"
+ fi
+
+Xvfb :99 -ac &
+
+./mvnw clean test -P ui \
 		-Dcucumber.options="--tags '""${TAGS}""'" \
 		-Dsyndesis.config.openshift.url="${URL}" \
 		-Dsyndesis.config.admin.username="${ADMIN_USERNAME}" \
 		-Dsyndesis.config.admin.password="${ADMIN_PASSWORD}" \
+		-Dsyndesis.config.ui.username="${UI_USERNAME}" \
+		-Dsyndesis.config.ui.password="${UI_PASSWORD}" \
 		-Dsyndesis.config.openshift.namespace="${NAMESPACE}" \
 		-Dsyndesis.config.openshift.namespace.lock=false \
 		-Dsyndesis.config.openshift.namespace.cleanup=false \
+	    $(echo ${UI_URL_CONFIG}) \
 		-Dmaven.surefire.debug="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xnoagent -Djava.compiler=NONE" \
-		-Dsyndesis.config.enableTestSupport=true
+		-Dsyndesis.config.enableTestSupport=true \
+		-Dsyndesis.config.ui.browser=firefox
 
 STATUS=$?
 
 echo "Status code from mvn command is: $STATUS . The test results will be stored into /test-run-results folder"
 
-mkdir /test-run-results
-mv ./rest-tests/target/cucumber/cucumber-html \
-   ./rest-tests/target/cucumber/cucumber-junit.xml \
-   ./rest-tests/target/cucumber/cucumber-mail \
-   ./rest-tests/target/cucumber/cucumber-report.json \
-   ./rest-tests/log/* \
+[ -d "/test-run-results" ] && sudo rm -rf /test-run-results/cucumber /test-run-results/log || sudo mkdir /test-run-results
+
+sudo mv ./ui-tests/target/cucumber \
+   ./ui-tests/log/ \
    /test-run-results
-cp /test-run-results/cucumber-junit.xml /test-run-results/junit.xml
 
 exit $STATUS
