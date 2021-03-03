71b0ca0dc29994ff31e5d7770c635c3da36f18b4
mkralik3 <mkralik@redhat.com>
2021-03-03 16:51:58 +0100

    Add dynamic wait to sns rest test

--- validation/src/main/java/io/syndesis/qe/validation/CommonSteps.java
+++ validation/src/main/java/io/syndesis/qe/validation/CommonSteps.java
@@ -93,6 +93,7 @@ public class CommonSteps {
     @When("deploy public oauth proxy")
     public void deployApiOauthProxy() {
         ResourceFactory.create(PublicOauthProxy.class);
+        TestUtils.sleepForJenkinsDelayIfHigher(5);
     }
 
     @Given("deploy FHIR server")
